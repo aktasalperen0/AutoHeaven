f9c8633c3c5866073189510618c5aba18e618cac
Alperen <aktasalperen0@gmail.com>
2024-05-15 12:47:35 +0300

    Template blades and their routes created

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,54 @@
 
 use Illuminate\Support\Facades\Route;
 
+//Route'lar test amaçlı function kullanıyor. Controller'a geçiş yapılacak.
+
 Route::get("/testTemplate", function (){
     return view("front.layouts.app");
+});
+
+Route::get("/about", function (){
+    return view("front.about");
+});
+
+Route::get("/blog", function (){
+    return view("front.blog");
+});
+
+Route::get("/blog-details", function (){
+    return view("front.blog-details");
+});
+
+Route::get("/car-details", function (){
+    return view("front.car-details");
+});
+
+Route::get("/cars", function (){
+    return view("front.cars");
+});
+
+Route::get("/contact", function (){
+    return view("front.contact");
+});
+
+Route::get("/faq", function (){
+    return view("front.faq");
+});
+
+Route::get("/index", function (){
+    return view("front.index");
+});
+
+Route::get("/team", function (){
+    return view("front.team");
+});
+
+Route::get("/terms", function (){
+    return view("front.terms");
+});
+
+Route::get("/testimonials", function (){
+    return view("front.testimonials");
 });
 
 Route::get('/', function () {
