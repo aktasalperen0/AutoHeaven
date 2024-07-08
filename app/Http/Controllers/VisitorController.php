60d09041e756762fd786ed8367f4cec54ad30a71
Alperen <aktasalperen0@gmail.com>
2024-07-08 13:41:35 +0300

    Controllers created

---
+++ app/Http/Controllers/VisitorController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class VisitorController extends Controller
+{
+    public function testTemplatePage(){
+        return view('front.layouts.app');
+    }
+
+    public function indexPage(){
+        return view('front.index');
+    }
+
+    public function aboutPage(){
+        return view("front.about");
+    }
+
+    public function blogPage(){
+        return view("front.blog");
+    }
+
+    public function blogDetailsPage(){
+        return view("front.blog-detail");
+    }
+
+    public function carDetailsPage(){
+        return view("front.car-detail");
+    }
+
+    public function carsPage(){
+        return view("front.cars");
+    }
+
+    public function contactPage(){
+        return view("front.contact");
+    }
+
+    public function faqPage(){
+        return view("front.faq");
+    }
+
+    public function teamPage(){
+        return view("front.team");
+    }
+
+    public function termsPage(){
+        return view("front.terms");
+    }
+
+    public function testimonialsPage(){
+        return view("front.testimonials");
+    }
+}
