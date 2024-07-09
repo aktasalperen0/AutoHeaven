db38730b6d76b74a137ea1dadb2c533387d03604
Alperen <aktasalperen0@gmail.com>
2024-07-09 13:05:02 +0300

    Brand and model seeders created

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,11 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
+        $this->call([
+            BrandSeeder::class,
+            ModelSeeder::class,
+        ]);
+
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
